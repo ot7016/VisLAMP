c0f949d183eaf5dace6395fa128f483be262319b
Ohta Naohiro <ohta.n.aa@m.titech.ac.jp>
2015-09-04 18:51:57 +0900

    PCP部分の描画を一つのwxGLCanvasではなく軸間をそれぞれwxGLCanvasで書く方式に変更

--- src/PCP.cpp
+++ src/PCP.cpp
@@ -7,66 +7,31 @@
 using namespace std;
  
  
-// some useful events to use
-void PCPPane::mouseMoved(wxMouseEvent& event) {}
-void PCPPane::mouseDown(wxMouseEvent& event) {}
-void PCPPane::mouseWheelMoved(wxMouseEvent& event) {}
-void PCPPane::mouseReleased(wxMouseEvent& event) {}
-void PCPPane::rightClick(wxMouseEvent& event) {}
-void PCPPane::mouseLeftWindow(wxMouseEvent& event) {}
-void PCPPane::keyPressed(wxKeyEvent& event) {}
-void PCPPane::keyReleased(wxKeyEvent& event) {}
- 
 PCPPane::PCPPane(wxWindow* parent, int* args,ReadData* d ) :
-    wxGLCanvas(parent, wxID_ANY, args, wxDefaultPosition, wxSize(800,600), wxFULL_REPAINT_ON_RESIZE)
+    wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(800,600), wxFULL_REPAINT_ON_RESIZE)
 {
- 	m_context = new wxGLContext(this);
     data = d;
-    setRate();
     int atr = data->getatr();
-    length = new float[atr];
     index = -1;
-    for(int i = 0; i< atr; i++){
-        length[i] = 1;
+    wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+  // wxGridSizer* sizer =   new wxGridSizer(1,atr-1,getWidth()/(atr-1),getHeight());
+    int len = getWidth()/(atr-1);
+    for(int i = 0 ;i< atr-1;i++){
+        PCPSub* sub =  new PCPSub(this,i,d,len);
+        sub->setSumLength(atr-1,getWidth()-atr+1);
+        sizer->Add(sub);
     }
-    
-    sumlength = atr;
+     sumlength = atr-1;
+    setRate(); 
+    SetSizer(sizer);
+    SetAutoLayout(true);
     // To avoid flashing on MSW
-    SetBackgroundStyle(wxBG_STYLE_CUSTOM);
 }
  
 PCPPane::~PCPPane(){
-	delete m_context;
-    delete[] length;
 }
- 
-void PCPPane::resized(wxSizeEvent& evt)
-{
-//	wxGLCanvas::OnSize(evt);
-   // Refresh();
-}
- 
-void PCPPane::prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y)
-{
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black Background
-    glEnable(GL_TEXTURE_2D);   // textures
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_BLEND);
-    glEnable(GL_LINE_SMOOTH);
-    glDisable(GL_DEPTH_TEST);
-    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
- 
-    glViewport(topleft_x, topleft_y, bottomrigth_x-topleft_x, bottomrigth_y-topleft_y);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
 
-    gluOrtho2D(topleft_x, bottomrigth_x, bottomrigth_y, topleft_y);
-    glMatrixMode(GL_MODELVIEW);
 
-    glLoadIdentity();
-}
- 
- 
 int PCPPane::getWidth()
 {
     return GetSize().x;
@@ -76,6 +41,7 @@ int PCPPane::getHeight()
 {
     return GetSize().y;
 }
+ 
 
 void PCPPane::setRate(){
    //各属性ごとに倍率があるので
@@ -83,35 +49,27 @@ void PCPPane::setRate(){
     rate = new float[dim];
     for(int i = 0;i< dim;i++ ){
         rate[i] = (getHeight()*3)/(4*(data->getDmax(i)-data->getDmin(i)));
-        // cerr << rate[i] << endl;
     }
+    int k = 0;
+    for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+        PCPSub *current = (PCPSub *)node->GetData();
+        current->setRate(k,k+1,rate[k],rate[k+1]);
+        k++;
+        }
+    
+
  //        std::cerr << xrate << std::endl;
    //         std::cerr << yrate << std::endl;
 }
 
-/*
-int PCPPane::getindex(int x, int y){
-    
-    
-    int index = -1;
-    const float min = 25;
-    float minnow = min; 
-    float d; 
-    for(int i = 0;i< data->getdim();i++){
-        d = pow( data->getA(i,0)* xrate -x,2)+pow( data->getA(i,1)* yrate - y,2);
-        if(d < minnow){
-            minnow = d;
-            index = i;
-        }
-    }
-    return index;
-    
-    return -1;
-} 
-*/
 
 void PCPPane::setIndex(int i){
     index = i;
+    for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+            PCPSub *current = (PCPSub *)node->GetData();
+            current->setIndex(i);
+        }
+
 }
 void PCPPane::refine(float** v){
     //TSPを解く
@@ -121,7 +79,21 @@ void PCPPane::refine(float** v){
         solveTSP(v,atr);
      else
         solveAngle(v,atr);
-     Refresh();
+   // wxWindowList & children = GetChildren();
+    int len[atr-1];
+    int k = 0;
+    for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+        PCPSub *current = (PCPSub *)node->GetData(); 
+        current->setSumLength(sumlength,getWidth());
+        current->Refresh();
+        len[k] = current->getWidth(); 
+        k++;
+        }
+        for(int i = 0;i < atr-1;i++){
+            std::cerr << len[i] << std::endl;
+        }
+         SetAutoLayout(true);
+         Show();
 }
 
 void PCPPane::solveTSP(float** v,int atr){
@@ -129,18 +101,29 @@ void PCPPane::solveTSP(float** v,int atr){
     ts->solve();
     data->setOrder(ts);
     sumlength = 0;
-    length = new float[atr];
-    if(data->isLenVar()){
-        for(int i = 0;i < atr-1; i++){
-            length[i] = ts->getlength(i+1);
-            sumlength += length[i];
+    int k = 0;
+    if(data->isLenVar()){ 
+        for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+            PCPSub *current = (PCPSub *)node->GetData();
+            float l = ts->getlength(k+1);
+            current->setLength(sumlength,l);
+            sumlength = sumlength + l;
+            int lindex = data->getOrder(k);
+            int rindex = data->getOrder(k+1);
+            current->setRate(lindex,rindex,rate[lindex],rate[rindex]);
+            k++;
         }
-        length[atr-1] = 0;
     }
     else{
-        for(int i = 0;i<atr;i++)
-            length[i] = 1;
-        sumlength = atr;
+         for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+            PCPSub *current = (PCPSub *)node->GetData();
+            current->setLength(k,1);
+            int lindex = data->getOrder(k);
+            int rindex = data->getOrder(k+1);
+            current->setRate(lindex,rindex,rate[lindex],rate[rindex]);
+            k++;
+        }
+        sumlength = atr-1;
     }
 
 }
@@ -202,25 +185,114 @@ void PCPPane::solveAngle(float** v,int atr){
         order[i] = c1.at(i).first;
     }
     data->setOrder(order);
-    length = new float[atr];
+ //   wxWindowList & children = GetChildren();
+    int k = 0;   
     if(data->isLenVar()){
         sumlength = 0;
-        for(int i = 0 ; i <atr-1 ;i++){
-            length[i] = acos(c1.at(i).second[0]* c1.at(i+1).second[0] + c1.at(i).second[1]* c1.at(i+1).second[1]);
-            sumlength = sumlength + length[i];
+       // for(int i = 0 ; i <atr-1 ;i++){
+           //length[i] = acos(c1.at(i).second[0]* c1.at(i+1).second[0] + c1.at(i).second[1]* c1.at(i+1).second[1]);
+            //sumlength = sumlength + length[i];
+        //}
+  
+        for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+            PCPSub *current = (PCPSub *)node->GetData();
+            float l = acos(c1.at(k).second[0]* c1.at(k+1).second[0] + c1.at(k).second[1]* c1.at(k+1).second[1]);
+            current->setLength(sumlength,l);
+            int lindex = c1.at(k).first;
+            int rindex = c1.at(k+1).first;
+            current->setRate(lindex,rindex,rate[lindex],rate[rindex]);
+            sumlength = sumlength + l;
+            k++;
         }
-        length[atr-1] = 0;
     }
     else{
-        for(int i = 0;i<atr;i++)
-            length[i] =1;
-        sumlength = atr;
+         for ( wxWindowList::Node *node = m_children.GetFirst(); node; node = node->GetNext() ){
+            PCPSub *current = (PCPSub *)node->GetData();
+            current->setLength(k,1);
+             int lindex = c1.at(k).first;
+            int rindex = c1.at(k+1).first;
+            current->setRate(lindex,rindex,rate[lindex],rate[rindex]);
+            k++;
+        }
+        sumlength = atr-1;
     }
 
 }
 
+// some useful events to use
+void PCPSub::mouseMoved(wxMouseEvent& event) {}
+void PCPSub::mouseDown(wxMouseEvent& event) {}
+void PCPSub::mouseWheelMoved(wxMouseEvent& event) {}
+void PCPSub::mouseReleased(wxMouseEvent& event) {}
+void PCPSub::rightClick(wxMouseEvent& event) {}
+void PCPSub::mouseLeftWindow(wxMouseEvent& event) {}
+void PCPSub::keyPressed(wxKeyEvent& event) {}
+void PCPSub::keyReleased(wxKeyEvent& event) {}
+
+PCPSub::PCPSub(wxWindow* parent, int num,ReadData* d,int size) :
+    wxGLCanvas(parent, wxID_ANY, NULL, wxPoint(num*size,0), wxSize(size,600), wxFULL_REPAINT_ON_RESIZE) //wxsize あとで変更
+{
+    m_context = new wxGLContext(this);
+    data = d;
+    index = -1;
+    length = 1; 
+    prelength = num; 
+    // To avoid flashing on MSW
+    SetBackgroundStyle(wxBG_STYLE_CUSTOM);
+}
+
+void PCPSub::prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black Background
+    glEnable(GL_TEXTURE_2D);   // textures
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_BLEND);
+    glEnable(GL_LINE_SMOOTH);
+    glDisable(GL_DEPTH_TEST);
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+ 
+    glViewport(topleft_x, topleft_y, bottomrigth_x-topleft_x, bottomrigth_y-topleft_y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluOrtho2D(topleft_x, bottomrigth_x, bottomrigth_y, topleft_y);
+    glMatrixMode(GL_MODELVIEW);
+
+    glLoadIdentity();
+}
+
+int PCPSub::getWidth()
+{
+    return GetSize().x;
+}
+void PCPSub::setLength(float p,float l){
+    prelength = p;
+    length = l;
+    //SetSize(l,getHeight());  
+}
+void PCPSub::setSumLength(float l,float w){
+    sumlength = l;
+    int size = (length/sumlength) *w;
+    SetSize(size  ,getHeight());
+    SetPosition(wxPoint((prelength/sumlength)*w,0));
+}
+void PCPSub::setIndex(int i){
+    index = i;
+}
+ 
+int PCPSub::getHeight()
+{
+    return GetSize().y;
+}
+void PCPSub::setRate(int l, int r,float left,float right){
+    leftindex = l;
+    rightindex = r; 
+    lrate = left;
+    rrate = right;
+}
+ 
  //本当は再描画のことも考えた関数設計にする
-void PCPPane::render(wxPaintEvent& evt)
+void PCPSub::render(wxPaintEvent& evt)
 {
     if(!IsShown()) return;
 
@@ -244,28 +316,26 @@ void PCPPane::render(wxPaintEvent& evt)
 
     glColor4f(0.0f,0.0f,0.0f,1.0f);
     glBegin(GL_LINES);
-    int dim = data-> getatr();
-    float len1 = 0;
-    float xwidth[dim]; 
+    //int dim = data-> getatr();
+    //float len1 = 0;
+   //float xwidth[dim]; 
     int ydown = getHeight()*7/8;
     int yup = getHeight()/8 ;
-    for(int i = 0; i< dim;i++){  //要素数
-        xwidth[i] = (len1/sumlength)*getWidth();
-    	glVertex3f(xwidth[i],ydown,0);
-    	glVertex3f(xwidth[i],yup,0);
-        len1 = len1 + length[i];  
-    }
+  //  for(int i = 0; i< dim;i++){
+        //xwidth[i] = (len1/sumlength)*getWidth();
+    	glVertex3f(getWidth(),ydown,0);
+    	glVertex3f(getWidth(),yup,0); 
     glEnd();
 
-    for(int i = 0 ;i< dim;i++){
-        glRasterPos2d(xwidth[i],ydown+40);
-        string str = data->getAtrName(data->getOrder(i));
+  //  for(int i = 0 ;i< dim;i++){
+        glRasterPos2d(getWidth()*0.5,ydown+40);
+        string str = data->getAtrName(data->getOrder(rightindex));
         int size = (int)str.size();
         for(int j = 0;j< size;j++){
             char ic = str[j];
             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,ic);
         }
-    }
+
 
     // 追加部分 点を書く
     glColor4f(0.0f,0.0f,1.0f,1.0f);
@@ -284,15 +354,14 @@ void PCPPane::render(wxPaintEvent& evt)
     SwapBuffers();
 }
 
-void PCPPane::draw(int i){
+void PCPSub::draw(int i){
     glBegin(GL_LINE_STRIP);
         float len = 0;
-    for(int j = 0; j< data->getatr(); j++){
-        int index = data->getOrder(j);
-        float l = (len/sumlength)*getWidth();
-        glVertex3f(l, (data->getDmax(index) - data->getD(i,index) ) *rate[index] + getHeight()/8 ,0);  
-        len = len + length[j];   
-    }
+// for(int j = 0; j< data->getatr(); j++){
+       // float l = (len/sumlength)*getWidth();  //rateの使用はあとで
+        glVertex3f(0, (data->getDmax(leftindex) - data->getD(i,leftindex) ) *lrate + getHeight()/8 ,0); 
+        glVertex3f(getWidth(), (data->getDmax(rightindex) - data->getD(i,rightindex) ) *rrate + getHeight()/8 ,0);  
+    
      glEnd();
 }
 
--- src/PCP.hpp
+++ src/PCP.hpp
@@ -17,24 +17,49 @@
 #ifndef pc 
 #define pc
 
-class PCPPane: public wxGLCanvas
-{
-    wxGLContext*	m_context;
-    ReadData* data;
+
+
+
+class PCPPane: public wxPanel{
+	
+	ReadData* data;
 public:
 	PCPPane(wxWindow* parent, int* args, ReadData* d);
 	virtual ~PCPPane();
-	void resized(wxSizeEvent& evt);
-	int getWidth();
-	int getHeight();
-	void render(wxPaintEvent& evt);
-	void draw(int i);
-	void prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y);
+	int getWidth();   //master
+	int getHeight();  //master
+	void solveTSP(float** v, int atr);   //master
+	void solveAngle(float** v, int atr); //master
+	void setIndex(int i);  //master 
+	void setRate();        //master 
 	void refine(float** v);
-	void solveTSP(float** v,int atr);
-	void solveAngle(float** v, int atr);
+	typedef std::pair<int,float*> ipair;
 	// events
-	void mouseMoved(wxMouseEvent& event);
+	private:
+	int index;
+	float* rate; 
+	//float* length;
+	float sumlength;
+};
+
+class PCPSub: public wxGLCanvas 
+{
+	wxGLContext* m_context;
+	ReadData* data;
+public:
+	 PCPSub(wxWindow* parent, int num, ReadData* d, int size);
+	//~ PCPSub();
+	int getWidth();   
+	int getHeight();  
+	void setRate(int l, int r, float left, float right);
+	void setLength(float p,float l);
+	void setSumLength(float l, float w);
+	void setIndex(int i);
+	void render(wxPaintEvent& evt);  //sub
+	void draw(int i);   //sub
+	void prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y);  //sub
+	// events
+	void mouseMoved(wxMouseEvent& event);   
 	void mouseDown(wxMouseEvent& event);
 	void mouseWheelMoved(wxMouseEvent& event);
 	void mouseReleased(wxMouseEvent& event);
@@ -42,11 +67,52 @@ public:
 	void mouseLeftWindow(wxMouseEvent& event);
 	void keyPressed(wxKeyEvent& event);
 	void keyReleased(wxKeyEvent& event);
-	void setIndex(int i);
-	//int getindex(int x, int y);
-	void setRate();
+	
+	DECLARE_EVENT_TABLE()
+	/* data */
+private:
+	int leftindex;
+	int rightindex;
+	float length;
+	float prelength;
+	float sumlength;
+	float rrate;
+	float lrate;
+	int index;
+};
+
+
+
+
+
+/*class PCPPane: public wxGLCanvas
+{
+    wxGLContext*	m_context;
+    ReadData* data;
+public:
+	PCPPane(wxWindow* parent, int* args, ReadData* d);
+	virtual ~PCPPane();
+	int getWidth();   //master
+	int getHeight();  //master
+	void render(wxPaintEvent& evt);  //sub
+	void draw(int i);   //sub
+	void prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y);  //sub
+	void refine(float** v); //master
+	void solveTSP(float** v,int atr);   //master
+	void solveAngle(float** v, int atr); //master
+	void setIndex(int i);  //master 
+	void setRate();        //master 
 	typedef std::pair<int,float*> ipair;
- 
+	// events
+	void mouseMoved(wxMouseEvent& event);   
+	void mouseDown(wxMouseEvent& event);
+	void mouseWheelMoved(wxMouseEvent& event);
+	void mouseReleased(wxMouseEvent& event);
+	void rightClick(wxMouseEvent& event);
+	void mouseLeftWindow(wxMouseEvent& event);
+	void keyPressed(wxKeyEvent& event);
+	void keyReleased(wxKeyEvent& event);
+	
 	DECLARE_EVENT_TABLE()
 private:
 	int index;
@@ -54,4 +120,8 @@ private:
 	float* length;
 	float sumlength;
 };
+*/
 #endif
+
+
+// PCPPaneと PCPsubにわける　masterが全体のパネル subが軸と軸の間を担当
